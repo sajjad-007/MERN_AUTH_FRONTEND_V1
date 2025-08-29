6fd128c0ee78c709c60080d95a6e94a5d418f617
sajjad-007 <sajjadhossain8123@gmail.com>
2025-08-29 08:46:27 +0600

    project started

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,24 @@
-import React, { useContext } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { toast } from "react-toastify";
-import { Context } from "../main";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Context } from '../main';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return <></>;
+  const [isAuthenticated, setIsAuthenticated, user, setUser] =
+    useContext(Context);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const navigateTo = useNavigate();
+  return <>
+    <div className='auth-form'>
+      
+    </div>
+  </>;
 };
 
 export default Login;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,17 +1,36 @@
-import React from "react";
-import "../styles/Auth.css";
+import React, { useContext, useState } from 'react';
+import '../styles/Auth.css';
+import Login from '../components/Login';
+import Register from '../components/Register';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
-  return <>
-    <div className="auth-page">
-      <div className="auth-container">
-        <div className="auth-toggle">
-          <button className="toggle-btn"></button>
-          <button className="toggle-btn"></button>
+  const { isAuthenticated } = useContext(Context);
+  const { isLogin, setIsLogin } = useState(true);
+  const navigateTo = useNavigate();
+  
+  if (isAuthenticated) {
+    navigateTo('/');
+  }
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-container">
+          <div className="auth-toggle">
+            <button
+              className={`toggle-btn ${isLogin ? 'active' : ''}`}
+              onClick={() => setIsLogin(true)}
+            ></button>
+            <button
+              className={`toggle-btn ${!isLogin ? 'active' : ''}`}
+              onClick={() => setIsLogin(false)}
+            ></button>
+          </div>
+          {isLogin ? <Login /> : <Register />}
         </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default Auth;
